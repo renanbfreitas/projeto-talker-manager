ec8ac359d7fdc8b35f37e7532189194ea1d01c2f
Renanbfreitas <renanbf1992@outlook.com>
2023-02-06 17:13:33 -0300

    feat: add req 4

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { returnToken } = require('./utils');
+const validateLogin = require('../middlewares/validateLogin');
 
 const loginRouter = express.Router();
 
-loginRouter.post('/', (_req, res) => {
+loginRouter.post('/', validateLogin, (_req, res) => {
   const tokenObj = { token: returnToken() };
 
   res.status(200).json(tokenObj);
