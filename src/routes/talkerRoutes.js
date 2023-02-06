6b03f487086360837d525bbfdf95020f36be500e
Renanbfreitas <renanbf1992@outlook.com>
2023-02-06 16:20:09 -0300

    feat: add req 2

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -10,6 +10,20 @@ router.get('/', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkers();
+
+  const talkerById = talkers
+    .find((talker) => talker.id === Number(id));
+
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(talkerById);
+});
+
 module.exports = {
   router,
 };
